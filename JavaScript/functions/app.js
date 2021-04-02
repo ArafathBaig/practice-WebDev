7c00ed01bae3fe803c79b47cc3d156f9424e00f7
Arafath Baig <arafath.moghul@gmail.com>
2021-04-02 11:10:41 +0530

    function weirdness

--- JavaScript/functions/app.js
+++ JavaScript/functions/app.js
@@ -9,3 +9,38 @@ function rollDie() {
 }
 
 callTwice(rollDie);
+
+function makeMysteryFunc() {
+
+    const rand = Math.random();
+
+    if (rand > 0.5) {
+        return function () {
+            console.log("Congrats, I am good funtion");
+            console.log("You win a million dollars")
+        }
+    } else {
+        return function () {
+            alert("Something")
+        }
+    }
+}
+
+
+function makeBetweenFunc(min, max) {
+
+    return function (num) {
+        return num >= min && num <= max;
+    }
+}
+const isChild = makeBetweenFunc(10, 50);
+isChild(15);
+
+
+// function isBetween(num) {
+//     return num >= 50 && num <= 100;
+// }
+
+// function isBetween2(num) {
+//     return num >= 1 && num <= 10;
+// }
