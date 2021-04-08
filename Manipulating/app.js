49e4672d15d3ba4746b0ac55088a1b895f047f1a
Arafath Baig <arafath.moghul@gmail.com>
2021-04-08 23:11:05 +0530

    changes

--- Manipulating/app.js
+++ Manipulating/app.js
@@ -4,7 +4,7 @@ for (let link of allLinks) {
     link.innerText = "I want Link";
 }
 
-for (let link of allLinks) {
-    link.style.color = 'rgb(0,108,134)';
-    link.style.textDecorationColor = 'magenta';
-}
+// for (let link of allLinks) {
+//     link.style.color = 'rgb(0,108,134)';
+//     link.style.textDecorationColor = 'magenta';
+// }
