fb5b359468b698cb25d185f50c063ad7e8c842a9
Arafath Baig <arafath.moghul@gmail.com>
2021-04-13 14:10:27 +0530

    stop propagation

--- Bubbling/app.js
+++ Bubbling/app.js
@@ -1,17 +1,20 @@
-const button = document.querySelector('#changeColor');
-const container = document.querySelector('#container');
-
-button.addEventListener('click', function (e) {
-    container.style.backgroundColor = makeRandColor();
-    e.stopPropagation();
-})
-container.addEventListener('click', function () {
-    container.classList.toggle('hide');
-})
-
-const makeRandColor = () => {
+function randomColor() {
     const r = Math.floor(Math.random() * 255);
     const g = Math.floor(Math.random() * 255);
     const b = Math.floor(Math.random() * 255);
-    return `rgb(${r}, ${g}, ${b})`;
+
+    const newColor = `rgb(${r}, ${g}, ${b})`;
+    return newColor;
 }
+
+const button = document.querySelector("#changeColor");
+const div = document.querySelector('#container')
+button.addEventListener('click', (e) => {
+    const color = randomColor();
+    document.body.style.backgroundColor = color;
+    e.stopPropagation();
+})
+
+div.addEventListener('click', function () {
+    div.classList.toggle('hide');
+})
