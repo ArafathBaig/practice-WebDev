540e74df1c40a21708f8ad8365ac256b05dde06d
arafathbaig <arafath.moghul@gmail.com>
2021-04-14 13:58:44 +0530

    score keeper part 1

---
+++ ScoreKeeper/app.js
@@ -0,0 +1,36 @@
+const p1Button = document.querySelector("#p1Button");
+const p2Button = document.querySelector("#p2Button");
+
+const p1Display = document.querySelector("#p1DisplayScore");
+const p2Display = document.querySelector("#p2DisplayScore");
+
+let p1Score = 0;
+let p2Score = 0;
+
+let isGameOver = false;
+
+let winningScore = 5;
+p1Button.addEventListener('click', function(e){
+
+    if(!isGameOver){
+        p1Score++;
+
+        if(p1Score === winningScore){
+            isGameOver = true;
+        }
+        p1Display.textContent = p1Score;
+    }
+});
+
+p2Button.addEventListener('click', function(e){
+
+    if(!isGameOver){
+        p2Score++;
+
+        if(p2Score === winningScore){
+            isGameOver = true;
+        }
+        p2Display.textContent = p1Score;
+    }
+});
+
