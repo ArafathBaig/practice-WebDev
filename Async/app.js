7dc6af9bb24d033081b6a6f246f0512b9c6b0937
arafathbaig <arafath.moghul@gmail.com>
2021-04-20 12:19:43 +0530

    callback hell

--- Async/app.js
+++ Async/app.js
@@ -1,5 +1,29 @@
-console.log("Sending request to server");
+// setTimeout(() => {
+//     document.body.style.backgroundColor = 'red';
+//     setTimeout(() => {
+//         document.body.style.backgroundColor = 'orange';
+//         setTimeout(() => {
+//             document.body.style.backgroundColor = 'yellow';
+//         }, 1000)
+//     }, 1000);
+// }, 1000);
 
-setTimeout(() => {
-    console.log("Here is your data from ther server.")
-}, 3000)
+const delayedColorChange = (newColor, delay, doNext) => {
+    setTimeout(() => {
+        document.body.style.backgroundColor = newColor;
+        doNext();
+    }, delay);
+}
+
+delayedColorChange('red', 1000, () => {
+    delayedColorChange('orange', 1000, () => {
+        delayedColorChange('yellow', 1000, () => {
+            delayedColorChange('blue', 1000, () => {
+
+            })
+        })
+    })
+});
+
+
+
