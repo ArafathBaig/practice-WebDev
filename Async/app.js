74de6bda784193cf54c46419ee382483f4ffc858
arafathbaig <arafath.moghul@gmail.com>
2021-04-21 12:00:45 +0530

    await keyword

--- Async/app.js
+++ Async/app.js
@@ -17,13 +17,26 @@ const delayedColorChange = (newColor, delay) => {
     });
 }
 
-delayedColorChange('red', 1000)
-    .then(() => delayedColorChange('orange', 1000))
-    .then(() => delayedColorChange('yellow', 1000))
-    .then(() => delayedColorChange('green', 1000))
-    .then(() => delayedColorChange('blue', 1000))
-    .then(() => delayedColorChange('indigo', 1000))
-    .then(() => delayedColorChange('violet', 1000))
-    .then(() => delayedColorChange('white', 1000))
+// delayedColorChange('red', 1000)
+//     .then(() => delayedColorChange('orange', 1000))
+//     .then(() => delayedColorChange('yellow', 1000))
+//     .then(() => delayedColorChange('green', 1000))
+//     .then(() => delayedColorChange('blue', 1000))
+//     .then(() => delayedColorChange('indigo', 1000))
+//     .then(() => delayedColorChange('violet', 1000))
+//     .then(() => delayedColorChange('white', 1000))
 
 
+async function rainbow() {
+    await delayedColorChange('red', 1000);
+    await delayedColorChange('orange', 1000);
+    await delayedColorChange('yellow', 1000);
+    await delayedColorChange('green', 1000);
+    await delayedColorChange('blue', 1000);
+    await delayedColorChange('indigo', 1000);
+    return "all done";
+}
+
+rainbow().then(() => {
+    console.log("Dumpeek")
+})
