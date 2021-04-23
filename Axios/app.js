0ba8335a418d71b2eda12d892a7218cace3bd4ce
arafathbaig <arafath.moghul@gmail.com>
2021-04-23 11:11:37 +0530

    axios

--- Axios/app.js
+++ Axios/app.js
@@ -10,3 +10,20 @@
 //     .catch(e => {
 //         console.log("OH NO! ERROR!", e)
 //     })
+
+axios.get('https://api.cryptonator.com/api/ticker/btc-usd')
+    .then(res => {
+        console.log(res.data.ticker.price);
+    })
+    .catch(err => {
+        console.log("ERROR", err);
+    })
+
+const fetcBitcoinPrice = async function () {
+    try {
+        const res = await axios.get("https://api.cryptonator.com/api/ticker/btc-usd");
+        console.log(res.data.ticker.price);
+    } catch (err) {
+        console.log(err);
+    }
+}
