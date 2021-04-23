af5ef6f97aa2828dbb66d96abf04e419dfe88823
arafathbaig <arafath.moghul@gmail.com>
2021-04-23 11:26:32 +0530

    axios dad joke

--- Axios/app.js
+++ Axios/app.js
@@ -11,14 +11,16 @@
 //         console.log("OH NO! ERROR!", e)
 //     })
 
-axios.get('https://api.cryptonator.com/api/ticker/btc-usd')
-    .then(res => {
-        console.log(res.data.ticker.price);
-    })
-    .catch(err => {
-        console.log("ERROR", err);
-    })
+// axios.get('https://api.cryptonator.com/api/ticker/btc-usd')
+//     .then(res => {
+//         console.log(res.data.ticker.price);
+//     })
+//     .catch(err => {
+//         console.log("ERROR", err);
+//     })
 
+const ul = document.querySelector("#jokes");
+const button = document.querySelector("button");
 const fetcBitcoinPrice = async function () {
     try {
         const res = await axios.get("https://api.cryptonator.com/api/ticker/btc-usd");
@@ -27,3 +29,25 @@ const fetcBitcoinPrice = async function () {
         console.log(err);
     }
 }
+
+const addNewJoke = async () => {
+    const joke = await getDadJoke();
+    const newLi = document.createElement("li");
+    newLi.append(joke);
+    ul.append(newLi);
+}
+
+const getDadJoke = async () => {
+    const headers = {
+        headers: {
+            Accept: 'application/json'
+        }
+    };
+    const res = await axios.get("https://icanhazdadjoke.com/", headers);
+    return res.data.joke;
+
+}
+
+button.addEventListener('click', async () => {
+    addNewJoke();
+})
