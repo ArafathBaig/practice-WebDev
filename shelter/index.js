8d36db263d988412a0eba9e42ec91cc25903e57d
arafathbaig <arafath.moghul@gmail.com>
2021-04-26 15:12:54 +0530

    npm install

--- shelter/index.js
+++ shelter/index.js
@@ -1,7 +1,7 @@
-const blue = require('./blue')
-const sadie = require('./sadie')
-const janet = require('./janet')
+const blue = require('./blue');
+const sadie = require('./sadie');
+const janet = require('./janet');
 
-const allCats = [blue, sadie, janet]
+const allCat = [blue, sadie, janet];
 
-module.exports = allCats;
+module.exports.all = allCat;
