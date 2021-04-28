377df151582b2134e41bd7f5ca1bd4a369e8d3a7
arafathbaig <arafath.moghul@gmail.com>
2021-04-28 17:34:50 +0530

    ejs with conditional and loops

--- Templating_Demo/index.js
+++ Templating_Demo/index.js
@@ -9,6 +9,16 @@ app.get('/', (req, res) => {
     res.render("home");
 })
 
+app.get('/cats', (req, res) => {
+    const cats = ['Blue', 'Rocket', 'Monty', 'Stephanie'];
+    res.render('cats', { cats });
+})
+
+app.get('/r/:subreddit', (req, res) => {
+    const { subreddit } = req.params;
+    res.render("subreddit", { subreddit });
+})
+
 app.get('/random', (req, res) => {
     const num = Math.floor(Math.random() * 10);
     res.render("random", { rand: num })
