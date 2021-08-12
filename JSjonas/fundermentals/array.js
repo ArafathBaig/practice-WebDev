b67ec39584f4e7b1b8659b32584444037ebb0da7
Arafath Baig <arafath.moghul@gmail.com>
2021-08-12 12:16:47 +0530

    objects

--- JSjonas/fundermentals/array.js
+++ JSjonas/fundermentals/array.js
@@ -1,32 +1,50 @@
-const friends = ['Michael'];
-friends.push('Jay');
+// const friends = ['Michael'];
+// friends.push('Jay');
 
-console.log(friends);
+// console.log(friends);
 
-friends.unshift('John');
+// friends.unshift('John');
 
-console.log(friends)
+// console.log(friends)
 
-//remove elements
+// //remove elements
 
-console.log(friends.indexOf('Jay'))
-friends.shift();
+// console.log(friends.indexOf('Jay'))
+// friends.shift();
 
-console.log(friends);
+// console.log(friends);
+
+// const namee = {
+//     firstName: 'Rasheed',
+//     lastName: 'Bhai',
+//     age: 2021 - 1997,
+//     job: 'Teacher',
+//     friends: ["Dumpak", "Silaki", "Gilli"]
+// }
+
+// console.log(namee);
+
+// console.log(namee['firstName'])
+
+// namee.location = 'America';
+// namee['insta'] = "@americanHero"
+
+// console.log(namee)
 
 const namee = {
     firstName: 'Rasheed',
     lastName: 'Bhai',
-    age: 2021 - 1997,
+    age: 1997,
     job: 'Teacher',
-    friends: ["Dumpak", "Silaki", "Gilli"]
+    friends: ["Dumpak", "Silaki", "Gilli"],
+    hasDriversLicense: true,
+    // calcAge: function (birhtYear) {
+    //     return 2037 - birhtYear;
+    // }
+    calcAge: function () {
+        console.log(this);
+        return 2037 - this.age;
+    }
 }
 
-console.log(namee);
-
-console.log(namee['firstName'])
-
-namee.location = 'America';
-namee['insta'] = "@americanHero"
-
-console.log(namee)
+console.log(namee.calcAge())
