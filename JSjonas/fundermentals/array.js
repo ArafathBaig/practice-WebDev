13e473690ab5ce172bd36e21d723e8f75ef464a9
Arafath Baig <arafath.moghul@gmail.com>
2021-08-12 12:08:28 +0530

    objects

--- JSjonas/fundermentals/array.js
+++ JSjonas/fundermentals/array.js
@@ -13,3 +13,20 @@ console.log(friends.indexOf('Jay'))
 friends.shift();
 
 console.log(friends);
+
+const namee = {
+    firstName: 'Rasheed',
+    lastName: 'Bhai',
+    age: 2021 - 1997,
+    job: 'Teacher',
+    friends: ["Dumpak", "Silaki", "Gilli"]
+}
+
+console.log(namee);
+
+console.log(namee['firstName'])
+
+namee.location = 'America';
+namee['insta'] = "@americanHero"
+
+console.log(namee)
