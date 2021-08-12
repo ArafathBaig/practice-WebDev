b9ccc659feecbdcd2f0caee2e69f2c7aba532713
Arafath Baig <arafath.moghul@gmail.com>
2021-08-12 11:57:30 +0530

    js array'

---
+++ JSjonas/fundermentals/array.js
@@ -0,0 +1,15 @@
+const friends = ['Michael'];
+friends.push('Jay');
+
+console.log(friends);
+
+friends.unshift('John');
+
+console.log(friends)
+
+//remove elements
+
+console.log(friends.indexOf('Jay'))
+friends.shift();
+
+console.log(friends);
