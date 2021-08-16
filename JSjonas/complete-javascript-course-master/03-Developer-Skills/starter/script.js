35155d5ec6c89093b53363e889481674bfb403ee
Arafath Baig <arafath.moghul@gmail.com>
2021-08-16 14:27:33 +0530

    finally

---
+++ JSjonas/complete-javascript-course-master/03-Developer-Skills/starter/script.js
@@ -0,0 +1,11 @@
+"use strict";
+
+const x = 23;
+
+const calcAge = (birthYear) => 2037 - birthYear;
+
+console.log(calcAge(15));
+
+console.log("This iab");
+console.log("This is it");
+console.log("this it it");
