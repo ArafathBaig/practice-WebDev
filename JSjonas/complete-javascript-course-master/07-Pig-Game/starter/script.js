c90958f561973fc14e64c30448ba7c594c851687
Arafath Baig <arafath.moghul@gmail.com>
2021-08-17 10:05:53 +0530

    saving the progress

--- JSjonas/complete-javascript-course-master/07-Pig-Game/starter/script.js
+++ JSjonas/complete-javascript-course-master/07-Pig-Game/starter/script.js
@@ -4,8 +4,60 @@ const player0 = document.getElementById(".player--0");
 const player1 = document.querySelector(".player--1");
 const score0El = document.getElementById("score--0");
 const score1El = document.getElementById("score--1");
+const current0El = document.getElementById("current--0");
+const current1El = document.getElementById("current--1");
 const diceEl = document.querySelector(".dice");
+const btnRollDice = document.querySelector(".btn--roll");
+const btnHold = document.querySelector(".btn--hold");
+const btnNew = document.querySelector(".btn--new");
 
+// if true - 1st player, if false 2nd player
+let currentPlayer = true;
+let score0 = 0,
+  score1 = 0;
+let currentScore0 = 0,
+  currentScore1 = 0;
 score0El.textContent = 0;
 score1El.textContent = 0;
 diceEl.classList.add("hidden");
+
+const displayCurrentScore = function () {
+  currentPlayer
+    ? (current0El.textContent = currentScore0)
+    : (current1El.textContent = currentScore1);
+};
+
+const incrementScore = function (score) {
+  currentPlayer ? (currentScore0 += score) : (currentScore1 += score);
+  displayCurrentScore();
+};
+
+const rollDice = function () {
+  let randomRoll = Math.trunc(Math.random() * 6) + 1;
+  let diceImg = `dice-${randomRoll}.png`;
+  diceEl.classList.remove("hidden");
+  diceEl.src = diceImg;
+  return randomRoll;
+};
+
+const switchPlayer = function () {
+  currentPlayer = !currentPlayer;
+};
+
+const resetScore = function () {
+  currentScore0 = 0;
+  currentScore1 = 0;
+  current0El.textContent = currentScore0;
+  current1El.textContent = currentScore1;
+};
+
+btnRollDice.addEventListener("click", function () {
+  let generatedNumber = Number(rollDice());
+
+  if (generatedNumber === 1) {
+    resetScore();
+    switchPlayer();
+  } else {
+    incrementScore(generatedNumber);
+  }
+});
