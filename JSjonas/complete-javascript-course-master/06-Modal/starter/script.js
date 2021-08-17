c8391c1388a7f23a77e9e2c2d9f5615f935a12e3
Arafath Baig <arafath.moghul@gmail.com>
2021-08-17 09:17:01 +0530

    modal

--- JSjonas/complete-javascript-course-master/06-Modal/starter/script.js
+++ JSjonas/complete-javascript-course-master/06-Modal/starter/script.js
@@ -1,1 +1,24 @@
-'use strict';
+"use strict";
+
+const modal = document.querySelector(".modal");
+const overlay = document.querySelector(".overlay");
+const btnCloseModal = document.querySelector(".close-modal");
+const btnsOpenModal = document.querySelectorAll(".show-modal");
+
+const hideModel = function () {
+  modal.classList.add("hidden");
+  overlay.classList.add("hidden");
+};
+
+const showModel = function () {
+  modal.classList.remove("hidden");
+  overlay.classList.remove("hidden");
+};
+
+for (let i = 0; i < btnsOpenModal.length; i++) {
+  btnsOpenModal[i].addEventListener("click", showModel);
+}
+
+btnCloseModal.addEventListener("click", hideModel);
+
+overlay.addEventListener("click", hideModel);
