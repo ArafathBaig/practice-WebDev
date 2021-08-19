3e3f4281a85462bd273106e6561304659a9a1db6
Arafath Baig <arafath.moghul@gmail.com>
2021-08-19 14:17:58 +0530

    this keyword

--- JSjonas/complete-javascript-course-master/08-Behind-the-Scenes/starter/script.js
+++ JSjonas/complete-javascript-course-master/08-Behind-the-Scenes/starter/script.js
@@ -1,1 +1,19 @@
 'use strict';
+
+const jonas = {
+  name: 'Jonas',
+  year: 1997,
+  calcAge: function () {
+    console.log(this);
+    return 2037 - this.year;
+  },
+};
+
+console.log(jonas.calcAge());
+
+const jj = {
+  year: 2017,
+};
+jj.calcAge = jonas.calcAge;
+
+console.log(jj.calcAge());
