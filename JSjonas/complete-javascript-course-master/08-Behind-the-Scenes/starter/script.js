15725379fe7cfef982c978f8aa69d8e875fa85b3
Arafath Baig <arafath.moghul@gmail.com>
2021-08-19 14:30:38 +0530

    function declaration vs arrow function

--- JSjonas/complete-javascript-course-master/08-Behind-the-Scenes/starter/script.js
+++ JSjonas/complete-javascript-course-master/08-Behind-the-Scenes/starter/script.js
@@ -1,19 +1,52 @@
 'use strict';
 
+// const jonas = {
+//   name: 'Jonas',
+//   year: 1997,
+//   calcAge: function () {
+//     console.log(this);
+//     return 2037 - this.year;
+//   },
+// };
+
+// console.log(jonas.calcAge());
+
+// const jj = {
+//   year: 2017,
+// };
+// jj.calcAge = jonas.calcAge;
+
+// console.log(jj.calcAge());
+
+var firstName = 'JJ';
+
 const jonas = {
   name: 'Jonas',
   year: 1997,
+  firstName: 'Schmeil',
   calcAge: function () {
     console.log(this);
-    return 2037 - this.year;
+    console.log(2037 - this.year);
+
+    //Solution 1
+    const self = this;
+
+    const isMillenial = function () {
+      console.log(self);
+      console.log(self.year);
+    };
+
+    isMillenial();
+
+    //Solution2
+    const isMillenialArrow = () => {
+      console.log(this);
+      console.log(this.year);
+    };
+
+    isMillenialArrow();
   },
+  greet: () => console.log(`Hey ${this.firstName}`),
 };
 
-console.log(jonas.calcAge());
-
-const jj = {
-  year: 2017,
-};
-jj.calcAge = jonas.calcAge;
-
-console.log(jj.calcAge());
+jonas.calcAge();
