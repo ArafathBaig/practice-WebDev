886626a8ff5cf026330603178fd1a8082cf87d29
Arafath Baig <arafath.moghul@gmail.com>
2021-08-19 14:44:06 +0530

    primitive vs objects type

--- JSjonas/complete-javascript-course-master/08-Behind-the-Scenes/starter/script.js
+++ JSjonas/complete-javascript-course-master/08-Behind-the-Scenes/starter/script.js
@@ -18,35 +18,56 @@
 
 // console.log(jj.calcAge());
 
-var firstName = 'JJ';
+// var firstName = 'JJ';
 
-const jonas = {
-  name: 'Jonas',
-  year: 1997,
-  firstName: 'Schmeil',
-  calcAge: function () {
-    console.log(this);
-    console.log(2037 - this.year);
+// const jonas = {
+//   name: 'Jonas',
+//   year: 1997,
+//   firstName: 'Schmeil',
+//   calcAge: function () {
+//     console.log(this);
+//     console.log(2037 - this.year);
 
-    //Solution 1
-    const self = this;
+//     //Solution 1
+//     const self = this;
 
-    const isMillenial = function () {
-      console.log(self);
-      console.log(self.year);
-    };
+//     const isMillenial = function () {
+//       console.log(self);
+//       console.log(self.year);
+//     };
 
-    isMillenial();
+//     isMillenial();
 
-    //Solution2
-    const isMillenialArrow = () => {
-      console.log(this);
-      console.log(this.year);
-    };
+//     //Solution2
+//     const isMillenialArrow = () => {
+//       console.log(this);
+//       console.log(this.year);
+//     };
 
-    isMillenialArrow();
-  },
-  greet: () => console.log(`Hey ${this.firstName}`),
+//     isMillenialArrow();
+//   },
+//   greet: () => console.log(`Hey ${this.firstName}`),
+// };
+
+// jonas.calcAge();
+
+const jessica = {
+  name: 'jessi',
 };
 
-jonas.calcAge();
+let otherJessica = new Object(jessica);
+otherJessica.name = 'John';
+
+//copying objects
+
+const jessica2 = {
+  firstName: 'Jessica',
+  lastName: 'Williams',
+  age: 27,
+  family: ['Alice', 'BOB'],
+};
+
+const jessicaCopy = Object.assign({}, jessica2);
+jessicaCopy.lastName = 'Cena';
+jessicaCopy.family.push('Dumpak');
+console.log(jessica2, jessicaCopy);
