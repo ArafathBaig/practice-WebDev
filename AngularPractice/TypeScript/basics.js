9829679a5e4934066d8d8234d693fb5c8e729f9a
Arafath Baig <arafath.moghul@gmail.com>
2021-09-08 18:18:24 +0530

    OOPs" git push "

--- AngularPractice/TypeScript/basics.js
+++ AngularPractice/TypeScript/basics.js
@@ -1,6 +1,14 @@
+var __spreadArray = (this && this.__spreadArray) || function (to, from, pack) {
+    if (pack || arguments.length === 2) for (var i = 0, l = from.length, ar; i < l; i++) {
+        if (ar || !(i in from)) {
+            if (!ar) ar = Array.prototype.slice.call(from, 0, i);
+            ar[i] = from[i];
+        }
+    }
+    return to.concat(ar || Array.prototype.slice.call(from));
+};
 var age = 24;
 var username = "Dumuku";
-//Array of strings
 var hobbies;
 hobbies = ["Sports", "Cooking"];
 var person;
@@ -9,6 +17,35 @@ person = {
     age: 32
 };
 var people;
-var course = "React - The Complete Guide";
-course = 1232;
+var course = 1232;
 console.log(course);
+function add(a, b) {
+    return a + b;
+}
+var value = add(3, 5);
+console.log(value);
+//Generics
+function insertAtBeginning(array, value) {
+    var newArray = __spreadArray([value], array, true);
+    return newArray;
+}
+var demoArray = [1, 2, 3];
+var updatedArray = insertAtBeginning(demoArray, -1);
+console.log(updatedArray);
+var Student = /** @class */ (function () {
+    function Student(first, last, age, courses) {
+        this.firstName = first;
+        this.lastName = last;
+        this.age = age;
+        this.courses = courses;
+    }
+    Student.prototype.enrol = function (courseName) {
+        this.courses.push(courseName);
+    };
+    Student.prototype.listCourse = function () {
+        return this.courses.slice();
+    };
+    return Student;
+}());
+var student = new Student("Max", "Schwarz", 32, ["Angular"]);
+console.log(student.listCourse());
--- AngularPractice/TypeScript/basics.ts
+++ AngularPractice/TypeScript/basics.ts
@@ -34,3 +34,56 @@ function add(a: number, b: number): number {
 
 var value = add(3, 5);
 console.log(value);
+
+//Generics
+function insertAtBeginning<T>(array: T[], value: T) {
+  const newArray = [value, ...array];
+  return newArray;
+}
+
+var demoArray = [1, 2, 3];
+var updatedArray = insertAtBeginning(demoArray, -1);
+console.log(updatedArray);
+
+class Student {
+  firstName: string;
+  lastName: string;
+  age: number;
+  private courses: string[];
+
+  constructor(first: string, last: string, age: number, courses: string[]) {
+    this.firstName = first;
+    this.lastName = last;
+    this.age = age;
+    this.courses = courses;
+  }
+
+  enrol(courseName: string) {
+    this.courses.push(courseName);
+  }
+
+  listCourse() {
+    return this.courses.slice();
+  }
+}
+
+var student = new Student("Max", "Schwarz", 32, ["Angular"]);
+console.log(student.listCourse());
+
+//Interfaces
+
+interface Human {
+  firstName: string;
+  age: number;
+
+  greet: () => void;
+}
+
+let max: Human;
+max = {
+  firstName: "Max",
+  age: 32,
+  greet() {
+    console.log("Hello!");
+  },
+};
