3fc53bb9192bf9fd7129413697b7efd83096e7d1
Arafath Baig <arafath.moghul@gmail.com>
2021-09-08 17:55:01 +0530

    function

---
+++ AngularPractice/TypeScript/basics.js
@@ -0,0 +1,14 @@
+var age = 24;
+var username = "Dumuku";
+//Array of strings
+var hobbies;
+hobbies = ["Sports", "Cooking"];
+var person;
+person = {
+    name: "Max",
+    age: 32
+};
+var people;
+var course = "React - The Complete Guide";
+course = 1232;
+console.log(course);
--- AngularPractice/TypeScript/basics.ts
+++ AngularPractice/TypeScript/basics.ts
@@ -1,27 +1,36 @@
-let age: number = 24;
-let username: string = "Dumuku";
+var age: number = 24;
+var username: string = "Dumuku";
 
 //Array of strings
 
-let hobbies: string[];
-
-hobbies = ["Sports", "Cooking"];
-
-let person: {
+type Person = {
   name: string;
   age: number;
 };
+
+var hobbies: string[];
+
+hobbies = ["Sports", "Cooking"];
+
+var person: Person;
 
 person = {
   name: "Max",
   age: 32,
 };
 
-let people: {
+var people: {
   name: string;
   age: number;
 }[];
 
-let course = "React - The Complete Guide";
+var course = 1232;
 
 console.log(course);
+
+function add(a: number, b: number): number {
+  return a + b;
+}
+
+var value = add(3, 5);
+console.log(value);
