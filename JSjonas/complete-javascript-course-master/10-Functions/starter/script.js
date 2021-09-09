e490eaf39f421afb08f348943993b70977c38e72
Arafath Baig <arafath.moghul@gmail.com>
2021-09-09 12:41:53 +0530

    call and apply

--- JSjonas/complete-javascript-course-master/10-Functions/starter/script.js
+++ JSjonas/complete-javascript-course-master/10-Functions/starter/script.js
@@ -44,20 +44,67 @@
 // checkIn(flight, jonas);
 // console.log(flight, jonas);
 
-const greet = function (greeting) {
-  return function (name) {
-    console.log(`${greeting} ${name}`);
-  };
+// const greet = function (greeting) {
+//   return function (name) {
+//     console.log(`${greeting} ${name}`);
+//   };
+// };
+
+// const greeterHey = greet('Hey');
+// greeterHey('Dumpak');
+
+// greet('Hello')('Silki');
+
+// const greetArrow = greeting => name => {
+//   console.log(`${greeting} ${name}`);
+// };
+
+// const heyArrow = greetArrow('Hellowww');
+// heyArrow('Justin');
+
+const lufthansa = {
+  airline: 'Lufthansa',
+  iataCode: 'LH',
+  bookings: [],
+
+  book(flightNum, name) {
+    console.log(
+      `${name} booked a seat on ${this.airline} flight ${this.iataCode}${flightNum}`
+    );
+
+    this.bookings.push({
+      flight: `${this.iataCode}${flightNum}`,
+      name,
+    });
+  },
 };
 
-const greeterHey = greet('Hey');
-greeterHey('Dumpak');
+lufthansa.book(239, 'John cena');
+lufthansa.book(333, 'The rock');
 
-greet('Hello')('Silki');
-
-const greetArrow = greeting => name => {
-  console.log(`${greeting} ${name}`);
+const eurowings = {
+  airline: 'Eurowings',
+  iataCode: 'EW',
+  bookings: [],
 };
 
-const heyArrow = greetArrow('Hellowww');
-heyArrow('Justin');
+const book = lufthansa.book;
+
+// book(23, 'Sarah Williams');
+
+book.call(eurowings, 23, 'Sarah Williams');
+book.call(lufthansa, 239, 'Roosevelt');
+
+const swiss = {
+  airline: 'Swiss Air Lines',
+  iataCode: 'LX',
+  bookings: [],
+};
+
+book.call(swiss, 583, 'Mary Cooper');
+
+//Apply method
+const flightData = [583, 'George Cooper'];
+book.apply(swiss, flightData);
+
+book.call(swiss, ...flightData);
