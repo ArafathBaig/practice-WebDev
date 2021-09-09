39425c3fa20533696c56eb03d0dc9ff96cb06176
Arafath Baig <arafath.moghul@gmail.com>
2021-09-09 11:39:15 +0530

    Pass by value vs pass by reference

--- JSjonas/complete-javascript-course-master/10-Functions/starter/script.js
+++ JSjonas/complete-javascript-course-master/10-Functions/starter/script.js
@@ -1,24 +1,45 @@
 'use strict';
 
-const bookings = [];
+// const bookings = [];
 
-const createBooking = function (
-  flightNum,
-  numPassengers = 1,
-  price = 199 * numPassengers
-) {
-  //   numPassengers = numPassengers || 1;
-  //   price = price || 199;
+// const createBooking = function (
+//   flightNum,
+//   numPassengers = 1,
+//   price = 199 * numPassengers
+// ) {
+//   //   numPassengers = numPassengers || 1;
+//   //   price = price || 199;
 
-  const booking = {
-    flightNum,
-    numPassengers,
-    price,
-  };
+//   const booking = {
+//     flightNum,
+//     numPassengers,
+//     price,
+//   };
 
-  console.log(booking);
-  bookings.push(booking);
+//   console.log(booking);
+//   bookings.push(booking);
+// };
+
+// createBooking('LH123');
+// createBooking('LH332', undefined, 5);
+
+const flight = 'LH234';
+
+const jonas = {
+  name: 'Max Max',
+  passport: 23434242,
 };
 
-createBooking('LH123');
-createBooking('LH332', undefined, 5);
+const checkIn = function (flightNum, passenger) {
+  flightNum = 'LH999';
+  passenger.name = 'Mr. ' + passenger.name;
+
+  if (passenger.passport === 23434242) {
+    alert('Check in');
+  } else {
+    alert('Wrong passport');
+  }
+};
+
+checkIn(flight, jonas);
+console.log(flight, jonas);
